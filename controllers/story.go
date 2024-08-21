c3bfa2b21767ed853af67d95e7962e556bdbdd15
Bismuth Flask <bismuthflask@protonmail.com>
2024-08-21 16:26:27 +0100

    Added ai gen endpoints to story controller

--- controllers/story.go
+++ controllers/story.go
@@ -6,17 +6,22 @@ import (
 	"crow/oraiplayground/services"
 	"crow/oraiplayground/templates"
 	"net/http"
+	"encoding/json"
+	"log"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 type Story struct {
 	StoryDatabaseService *services.StoryDatabase
+	AiServerService *services.AiServer
 }
 
 func storyRetrieveState(ctx context.Context) *Story {
 	return &Story{
 		StoryDatabaseService: ctx.Value(services.StoryDatabaseCtxKey).(*services.StoryDatabase),
+		AiServerService: ctx.Value(services.AiServerCtxKey).(*services.AiServer),
 	}
 }
 
@@ -35,158 +40,11 @@ func postSettings(w http.ResponseWriter, r *http.Request) {
 	//templates.Settings(w, &ctx)
 }
 
-func getBlockEditor(w http.ResponseWriter, r *http.Request) {
-	//state := storyRetrieveState(r.Context())
-	//r.ParseForm()
-
-	//story := state.StoryDatabaseService.LockForRead("default")
-	//block := story.GetPromptBlock(r.Form.Get("block"))
-
-	//blockEditor := templates.PromptBlockEditor{
-	//	Mode: story.Mode,
-	//	EditorId: r.Form.Get("eid"),
-	//	SectionOptions: models.PromptBlockSectionOptions(),
-	//	RoleOptions: models.PromptBlockRoleOptions(),
-	//}
-	//if block != nil {
-	//	blockEditor.Name = block.Name
-	//	blockEditor.Text = block.Text
-	//	utils.SetSelection(blockEditor.RoleOptions, string(block.Role))
-	//}
-
-	//_ = templates.PromptBlockEditor(w, &blockEditor)
-}
-
-func putBlockEditor(w http.ResponseWriter, r *http.Request) {
+func getPlaygroundList(w http.ResponseWriter, r *http.Request) {
 	state := storyRetrieveState(r.Context())
-	r.ParseForm()
-
-	story := state.StoryDatabaseService.LockForWrite("default")
-
-	updateBlock := models.PromptBlock{
-		Name: r.Form.Get("block"),
-		Role: models.PromptRole(r.PostFormValue("role")),
-		Text: r.PostFormValue("text"),
-	}
-
-	// TODO
-	err := story.UpdatePromptBlock(r.Form.Get("block"), updateBlock)
-	if err != nil {
-		return
-	}
-}
-
-func postPromptBlock(w http.ResponseWriter, r *http.Request) {
-	//state := storyRetrieveState(r.Context())
-	//r.ParseForm()
-
-	//newBlock := models.PromptBlock{
-	//	Name: r.PostFormValue("name"),
-	//	IsGroup: r.PostFormValue("type") == "group",
-	//}
-
-	//_ = state.StoryService.NewPromptBlock(newBlock)
-
-	//ctx := state.StoryService.PromptListComponent.IntoTmplModel()
-	//templates.PromptBlockList(w, &ctx)
-}
-
-func deletePromptBlock(w http.ResponseWriter, r *http.Request) {
-//	state := storyRetrieveState(r.Context())
-//	r.ParseForm()
-//
-//	blockNames := r.PostFormValue("blocks")
-//	blockNameList := strings.Split(blockNames, ",")
-//	state.StoryService.DeletePromptBlocks(blockNameList)
-//
-//	ctx := state.StoryService.PromptListComponent.IntoTmplModel()
-//	templates.PromptBlockList(w, &ctx)
-}
-
-func postAiRequest(w http.ResponseWriter, r *http.Request) {
-	//state := storyRetrieveState(r.Context())
-	//err := r.ParseMultipartForm(10000)
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusInternalServerError)
-	//	return
-	//}
-
-	//continueValue := r.PostFormValue("continue")
-
-	//reqInfo := state.StoryService.RequestPrompt(continueValue)
-	//jsonData, err := json.Marshal(&reqInfo)
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusInternalServerError)
-	//	return
-	//}
-	//r.Header.Add("Content-Type", "application/json")
-	//w.Write(jsonData)
-}
-
-func getAiRequest(w http.ResponseWriter, r *http.Request) {
-	//state := storyRetrieveState(r.Context())
-	//idStr := r.URL.Query().Get("id")
-	//id, err := strconv.ParseInt(idStr, 10, 64)
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusBadRequest)
-	//	return
-	//}
-
-	//response := state.StoryService.GetPromptRequest(id)
-	//jsonData, err := json.Marshal(&response)
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusInternalServerError)
-	//	return
-	//}
-	//r.Header.Add("Content-Type", "application/json")
-	//w.Write(jsonData)
-}
-
-func deleteAiRequest(w http.ResponseWriter, r *http.Request) {
-	//state := storyRetrieveState(r.Context())
-	//idStr := r.URL.Query().Get("id")
-	//id, err := strconv.ParseInt(idStr, 10, 64)
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusBadRequest)
-	//	return
-	//}
-
-	//err = state.StoryService.CancelPromptRequest(id)
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusBadRequest)
-	//}
-}
-
-type PromptResponse struct {
-	Prompt string `json:"prompt"`
-}
-
-func getPromptPreview(w http.ResponseWriter, r *http.Request) {
-	//state := storyRetrieveState(r.Context())
-	//prompt := state.StoryService.GetPrompt()
-
-	//res := PromptResponse{
-	//	Prompt: prompt,
-	//}
-	//jsonData, err := json.Marshal(&res)
-	//if err != nil {
-	//	http.Error(w, err.Error(), http.StatusInternalServerError)
-	//	return
-	//}
-	//r.Header.Add("Content-Type", "application/json")
-	//w.Write(jsonData)
-}
-
-func putActivatePromptBlock(w http.ResponseWriter, r *http.Request) {
-	//state := storyRetrieveState(r.Context())
-	//r.ParseForm()
-
-	//name := r.PostFormValue("name")
-	//active := r.PostFormValue("active") == "on"
-	//state.StoryService.ActivatePromptBlock(name, active)
-
-	//ctx := state.StoryService.PromptListComponent.IntoTmplModel()
-	//templates.PromptBlockList(w, &ctx)
+	vars := mux.Vars(r)
+	story := state.StoryDatabaseService.LockForRead(vars["storyName"])
+	templates.PlaygroundBlockList(w, story)
 }
 
 func getPromptInfo(w http.ResponseWriter, r *http.Request) {
@@ -196,8 +54,72 @@ func getPromptInfo(w http.ResponseWriter, r *http.Request) {
 	templates.PromptInfo(w, story)
 }
 
+func postAiRequest(w http.ResponseWriter, r *http.Request) {
+	state := storyRetrieveState(r.Context())
+	vars := mux.Vars(r)
+	story := state.StoryDatabaseService.LockForRead(vars["storyName"])
+
+	prompt := models.BlocksIntoPrompt(story.GetEnabledBlocks(), story.ModelSettings.Template)
+
+	reqInfo := state.AiServerService.IssueRequest(
+		story.ModelSettings.Model,
+		story.ModelSettings.GetParameters(),
+		prompt,
+	)
+
+	jsonData, err := json.Marshal(&reqInfo)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	r.Header.Add("Content-Type", "application/json")
+	w.Write(jsonData)
+}
+
+func getAiRequest(w http.ResponseWriter, r *http.Request) {
+	state := storyRetrieveState(r.Context())
+	vars := mux.Vars(r)
+
+	id, err := strconv.ParseInt(vars["requestId"], 10, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	ch := state.AiServerService.RequestChannel(id)
+	data := <-ch
+	jsonData, err := json.Marshal(&data)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	r.Header.Add("Content-Type", "application/json")
+	w.Write(jsonData)
+}
+
+func deleteAiRequest(w http.ResponseWriter, r *http.Request) {
+	state := storyRetrieveState(r.Context())
+	vars := mux.Vars(r)
+
+	id, err := strconv.ParseInt(vars["requestId"], 10, 64)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	err = state.AiServerService.CancelRequest(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+}
+
 func InstallStoryController(router *mux.Router) {
 	router.HandleFunc("/", getIndex).Methods("GET")
 	r := router.PathPrefix("/story/{storyName}/").Subrouter()
 	r.HandleFunc("/promptInfo", getPromptInfo).Methods("GET")
+	r.HandleFunc("/playground/list", getPlaygroundList).Methods("GET")
+	r.HandleFunc("/gen", postAiRequest).Methods("POST")
+	r.HandleFunc("/gen/{requestId}", getAiRequest).Methods("GET")
+	r.HandleFunc("/gen/{requestId}", deleteAiRequest).Methods("DELETE")
 }
+
